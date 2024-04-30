544dc417dcd457ba5eac6ff296844585f4373b1e
Jason Cockroft <jcockroft96@gmail.com>
2024-04-30 13:39:29 -0700

    Fix formatting

--- resources.tf
+++ resources.tf
@@ -1,4 +1,4 @@
-##################################################################################
+#############################JC####################################################
 # PROVIDERS
 ##################################################################################
 
@@ -17,7 +17,7 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    Environment = var.environment    
+    Environment = var.environment
   }
 }
 
@@ -42,23 +42,23 @@ module "main" {
 }
 # __generated__ by Terraform from "sg-087392b502e8fda73"
 resource "aws_security_group" "ingress" {
- description = "Security group with no ingress rule"
- egress = [{
- cidr_blocks      = ["0.0.0.0/0"]
- description      = ""
- from_port        = 0
- ipv6_cidr_blocks = []
- prefix_list_ids  = []
- protocol         = "-1"
- security_groups  = []
- self             = false
- to_port          = 0
- }]
- ingress                = []
- name                   = "no-ingress-sg"
- name_prefix            = null
- revoke_rules_on_delete = null
- tags                   = local.common_tags
- tags_all               = {}
- vpc_id                 = module.main.vpc_id
- }
+  description = "Security group with no ingress rule"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress                = []
+  name                   = "no-ingress-sg"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = local.common_tags
+  tags_all               = {}
+  vpc_id                 = module.main.vpc_id
+}
